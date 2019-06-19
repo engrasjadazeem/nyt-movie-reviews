f139391de4c50178e8ccd8ae4ea2b5d627dcad22
engrasjadazeem <engrasjadazeem@gmail.com>
2019-06-19 23:38:48 +0200

    Search implemented

--- src/components/header.js
+++ src/components/header.js
@@ -6,7 +6,7 @@ class Header extends React.Component {
       <div className="float-left mt-3">
         <h1 className="display-4">Movie Reviews</h1>
         <p className="text-muted float-left">
-          Powered by <a href="https://developer.nytimes.com/apis" target="_blank">Newyork Times API</a>
+          Powered by <a href="https://developer.nytimes.com/apis" target="_blank" rel="noopener noreferrer">Newyork Times API</a>
         </p>
       </div>
     ); 
--- src/components/movieReview.js
+++ src/components/movieReview.js
@@ -14,12 +14,18 @@ class MovieReview extends React.Component {
     this.handleSearchBoxChange = this.handleSearchBoxChange.bind(this);
   }
 
-  handleSearch() {
-    console.log(this.state.movieName);
+  async handleSearch() {
+    const res = await fetch(`https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=${this.state.movieName}&api-key=${config.NYTAPIKey}`);
+    const data = await res.json();
+    this.setState({ reviews: data.results });
   }
 
   handleSearchBoxChange(event) {
     this.setState({ movieName: event.target.value});
+    // If nothing in search box revert to previous
+    if (event.target.value === '') {
+      this.fetchMoviesData();
+    }
   }
 
   componentWillMount() {
@@ -29,19 +35,12 @@ class MovieReview extends React.Component {
   async fetchMoviesData() {
     const res = await fetch(`https://api.nytimes.com/svc/movies/v2/reviews/picks.json?api-key=${config.NYTAPIKey}`);
     const data = await res.json();
-
-    console.log(`Reviews data fetched, Count: ${data.num_results}`);
-    console.log(data);
-    // return {
-    //   reviews: data.results
-    // }
     this.setState({ reviews: data.results });
   }
 
   render() {
     return (
       <div>
-        {this.state.movieName}
         <div className="input-group mb-3">
           <input 
             type="text"
@@ -64,15 +63,20 @@ class MovieReview extends React.Component {
 
       <div className="row">
         {this.state.reviews.map(review => (
-        <div className="col-lg-4 col-md-6 col-sm-12 float-left d-flex mb-3">
-          <div className="card" key={review.link.url} >
-            <img className="card-img-top" src={review.multimedia.src} alt="Card cap" />
+        <div className="col-lg-4 col-md-6 col-sm-12 float-left d-flex mb-3" key={review.link.url}>
+          <div className="card">
+            {(review.multimedia && review.multimedia.src) ?
+            (<img className="card-img-top" src={review.multimedia.src} alt="Card cap" />) :
+            (<img className="card-img-top" src="https://i2.wp.com/candidcover.net/wp-content/uploads/2017/05/nyc1-2.png?zoom=2&resize=286%2C180&ssl=1" alt="Card cap"/>)
+            }
             <div className="card-body">
               <h5 className="card-title">{review.display_title}</h5>
               <p className="card-text">
-                <span className="text-small">{review.summary_short}</span>
+                {(review.summary_short) ?
+                (<span className="text-small">{review.summary_short}</span>) :
+                (<span className="text-small">No summary found for this review, Please click Read More to see the details</span>)}
               </p>
-              <a href={review.link.url} target="_blank" className="btn btn-sm btn-primary">Read more...</a>
+              <a href={review.link.url} target="_blank" rel="noopener noreferrer" className="btn btn-sm btn-primary">Read more...</a>
             </div>
           </div>
         </div>
